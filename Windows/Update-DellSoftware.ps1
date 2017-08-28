5dd0895864316fdd826e192e0050486b5887235d
Erwin Wildenburg <e.wildenburg@constant.it>
2017-08-28 18:02:27 +0200

    Auto update DCU when it is out of date

--- Windows/Update-DellSoftware.ps1
+++ Windows/Update-DellSoftware.ps1
@@ -1,3 +1,8 @@
+# Constants
+$dcuName = "Dell Command | Update"
+$dcuVersion = "2.3.1"
+$dcuDlLoc = "https://downloads.dell.com/FOLDER04358530M/1/Dell-Command-Update_X79N4_WIN_2.3.1_A00.EXE"
+
 # Get the device information
 $manufacturer = (Get-WmiObject -Class Win32_ComputerSystem).Manufacturer
 $model = (Get-WmiObject -Class Win32_ComputerSystem).Model
@@ -8,15 +13,17 @@ if ($manufacturer -like "Dell*" -and (($model -like "OptiPlex*") -or ($model -li
 {
     $isInstalled32 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -eq "Dell Command | Update" }
     $isInstalled64 = Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -eq "Dell Command | Update" }
-    if (($isInstalled32.DisplayName -ne "Dell Command | Update") -and ($isInstalled64.DisplayName -ne "Dell Command | Update"))
+    if ((($isInstalled32.DisplayName -ne $dcuName) -and ($isInstalled64.DisplayName -ne $dcuName)) `
+     -or (($isInstalled32.DisplayVersion -ne $dcuVersion) -and $isInstalled64.DisplayVersion -ne $dcuVersion))
     {
+
         Write-Host "Installing Dell Command | Update..."
 
         # Download Dell Command Update
-        Invoke-WebRequest -Uri "https://downloads.dell.com/FOLDER04358530M/1/Dell-Command-Update_X79N4_WIN_2.3.1_A00.EXE" -OutFile "C:\Temp\Dell-Command-Update_X79N4_WIN_2.3.1_A00.EXE" -UseBasicParsing
+        Invoke-WebRequest -Uri $dcuDlLoc -OutFile "C:\Temp\DCU.exe" -UseBasicParsing
 
         # Install Dell Command Update
-        & "C:\Temp\Dell-Command-Update_X79N4_WIN_2.3.1_A00.EXE" /s
+        & "C:\Temp\DCU.exe" /s
     }
     else
     {
